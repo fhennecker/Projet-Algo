2865ba263f4552f5cbf64cbfa05215bfb1a67f5f
Florentin Hennecker <florentin.hennecker@gmail.com>
2013-11-27 20:02:07 +0100

    Methodes rajoutees : int getDebt(Frat), boolean hasDebt(Frat), boolean isFrat(Frat), void addDebt(Frat, int), void deleteDebt(Frat), void changeDebt(Frat, int) -> int peut etre positif ou negatif.

--- Frat.java
+++ Frat.java
@@ -9,29 +9,102 @@ public class Frat {
 	////////// GETTERS + SETTERS //////////
 	public String getName() { return _name; }
 	public void setName(String name) { _name = name; }
+	public boolean isFrat(String fratName) { return (fratName == getName()); }
 
 	public int getBudget() { return _budget; }
 	public void setBudget(int budget) { _budget = budget; }
 
-	public Debt[] getDebtList() { return _debtList; }
-	public void setDebtList(Vector<Debt> debtList) { _debtList = debtList; }
+	private Vector<Debt> getDebtList() { return _debtList; }
+	private Debt getDebtAtIndex(int i){ 
+		return getDebtList().get(i);
+	}
+
+	public int getDebt(Frat creditor){
+		try {
+			int i = 0;
+			while (getDebtAtIndex(i).getCreditor() != creditor){
+				++i;
+			}
+			return (getDebtAtIndex(i).getAmount());
+		} 
+		catch (ArrayIndexOutOfBoundsException e){
+			// No debt has been found
+			return 0;
+		} 
+	}
+
+	public void setDebt(Frat creditor, int newAmount){
+		try {
+			int i = 0;
+			while (getDebtAtIndex(i).getCreditor() != creditor){
+				++i;
+			}
+			getDebtAtIndex(i).setAmount(newAmount);
+		} 
+		catch (ArrayIndexOutOfBoundsException e){
+			System.out.println("Error while changing debt : creditor could not be found.");
+		}
+	}
+
+	public boolean hasDebt(Frat otherFrat){
+		/** Returns true if this frat has debts for other frat, false otherwise.*/
+		return (getDebt(otherFrat) != 0);
+	}
 
 	////////// CONSTRUCTOR //////////
 	public Frat(String name, int budget){
 		setName(name);
 		setBudget(budget);
-		_d
-	}
-	public Frat(String name, int budget, Debt[] debtList){
-		setName(name);
-		setBudget(budget);
-		setDebtList(debtList);
+		_debtList = new Vector<Debt>();	
 	}
 
 	////////// WORK METHODS //////////
 	public void addDebt(Frat creditor, int amount){
+		_debtList.add(new Debt(creditor, amount));
+	}
+
+	public void deleteDebt(Frat creditor){
+		/** deleteDebt deletes debt of a creditor. */
+		try {
+
+			int i = 0;
+			while (getDebtAtIndex(i).getCreditor() != creditor){
+				++i;
+			}
+			getDebtList().remove(i);
+
+		} catch (ArrayIndexOutOfBoundsException e){
+			System.out.println("Error while deleting debt : creditor could not be found.");
+		}
+	}
+
+	public void changeDebt(Frat creditor, int amount){
+		/** changeDebt allows to add an amount to creditor's debt. Positive and
+			negative amounts are accepted. */
+		try{
+			setDebt(creditor, getDebt(creditor) + amount);
+		}
+		catch ( ArrayIndexOutOfBoundsException e ) {
+			System.out.println("Error while changing debt : creditor could not be found.");
+		}
+	}
+	/*
+	public static void main(String[] args) {
+								///////////////////////////////////////////////////////////////////// REMOVE MAIN
+	
+		Frat f = new Frat("CI", 100);
+		Frat g = new Frat("CP", 200);
+		System.out.println(f.hasDebt(g));
+		f.addDebt(g, 10);
+		System.out.println(f.getDebt(g));
+		f.changeDebt(g, 5);
+		System.out.println(f.getDebt(g));
+		System.out.println(f.hasDebt(g));
+
+
 
 	}
+	*/
 }
 
 class Debt {
@@ -45,4 +118,10 @@ class Debt {
 
 	public Frat getCreditor() { return _creditor; }
 	public void setCreditor(Frat creditor) { _creditor = creditor; }
+
+	////////// CONSTRUCTOR //////////
+	public Debt(Frat creditor, int amount){
+		setCreditor(creditor);
+		setAmount(amount);
+	}
 }
